2cbaf3af225ceaf26e194ed8cfe89168c2cce877
Dazai <kumardhirend999@gmail.com>
2025-05-11 18:23:40 +0530

    Bot.py

--- Bot.py
+++ Bot.py
@@ -3,22 +3,19 @@ from telebot import types
 ttoken = os.environ['TOKEN']
 bot = telebot.TeleBot(ttoken)
 
-# Github: https://github.com/PluginX
-# Telegram: https://t.me/Plugin
-
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
 
 
             key = types.InlineKeyboardMarkup()
-            b1 = types.InlineKeyboardButton(text='Channel', url='https://t.me/CodeLeak')
-            b2 = types.InlineKeyboardButton(text='apis.red', url='https://apis.red')
+            b1 = types.InlineKeyboardButton(text='Channel', url='https://t.me/Dazai_hoster_updates')
+            b2 = types.InlineKeyboardButton(text='Group', url='https://t.me/Dazai_hoster_chat')
             first = message.chat.first_name
 
             key.add(b1)
             key.add(b2)
 
-            bot.send_photo(message.chat.id, 'https://t.me/thuuu/8',
+            bot.send_photo(message.chat.id, 'https://t.me/MarvelXVenom',
                            caption=f'Hi {first}.\nWelcome to our free python host bot\nMade By: @Plugin\n\nPlease see the photo!\n*Send your python file first!*\n\n/help\n *To get the help page*\n\n/pip + Library name\n *To install a Library*\n\n/run + Your file Id\n *To run your bot!*',
                            parse_mode='markdown', reply_markup=key)
 
@@ -72,7 +69,7 @@ def Get(message):
                 try:
 
                     key = types.InlineKeyboardMarkup()
-                    b1 = types.InlineKeyboardButton(text='Coder', url='https://t.me/Plugin')
+                    b1 = types.InlineKeyboardButton(text='Coder', url='https://t.me/Dazai_hoster_chat')
                     key.add(b1)
 
                     bot.send_message(message.chat.id, text="*Help Page* 📑\n1- Drag your python file to the bot\n2- Then the bot will give you *File ID*\n\n3- Then install your Librarys\n 🔍 using /pip + *library name*\n\n4- Run your bot\n 🔍 using /run + *File iD*\n\n*Contact*: @Plugin", parse_mode='markdown', reply_markup=key)
